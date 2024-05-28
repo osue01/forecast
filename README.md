a65f401f45bea6ebffd5535955dcc07d99f10b39
Olivia <osue01@gmail.com>
2024-05-27 21:09:57 -1000

    readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # forecast
+
+Description:
+Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+Credits:
+Thanks to Aster Oliva, John Young, and Nick Gambino
+https://blog.logrocket.com/localstorage-javascript-complete-guide/
+https://openweathermap.org/
+
+
+
--- script.js
+++ script.js
@@ -1,25 +1,89 @@
-const requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=Sanfrancisco&limit=1&appid=70fc9519257c8157df1f3d00152ce8af`;
+const APIKEY = "70fc9519257c8157df1f3d00152ce8af";
+let currentStored = [];
+let forecastStored = [];
 
-async function checkWeather() {
-  const response = await fetch(requestURL);
-  var data = await response.json();
+//form submission
+document.querySelector("form").addEventListener("submit", searchbtn);
 
-  console.log(data);
+//handle searchbtn
+async function searchbtn(event) {
+  event.preventDefault();
+  const myCity = document.getElementById("cityInput").value;
+  const requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${myCity}&limit=1&appid=${APIKEY}`;
+  const coord = await getCoord(requestURL);
+
+  const currentURL = `http://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&units=imperial&appid=${APIKEY}`;
+  //need function like getCoord that will return 1 day weather
+  currentStored = [];
+  await getWeather(currentURL);
+  console.log("weather stored", currentStored);
+
+  //then new api url to fetch 5 day forecast
+  const forecastURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${coord.lat}&lon=${coord.lon}&appid=${APIKEY}`;
+  await getForecast(forecastURL);
 }
 
-checkWeather();
-//form submission
-document.querySelector("form").addEventListener("submit", function (event) {
-  event.preventDefault();
-});
+//call api
+async function getCoord(requestURL) {
+  const response = await fetch(requestURL);
+  const data = await response.json();
+  if (data.length > 0) {
+    return data[0];
+  }
+  return null;
+}
 
-// fetch(requestURL)
-//   .then(function (response) {
-//     // In order to use the data, it must first be parsed. Use .json() when the
-//     // API response format is JSON.
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log("Fetch Response \n-------------");
-//     console.log(data);
-//   });
+// call 1 day weather
+async function getWeather(currentURL) {
+  try {
+    const weatherResponse = await fetch(currentURL);
+    const weatherData = await weatherResponse.json();
+    console.log("weather data", weatherData);
+    // console.log("weathr response", weatherResponse);
+    const icon = weatherData.weather[0].icon;
+    const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    const t = weatherData.main.temp;
+    const w = weatherData.wind.speed;
+    const h = weatherData.main.humidity;
+    currentStored.push({
+      img: iconURL,
+      t: t,
+      w: w,
+      h: h,
+    });
+
+    return weatherData[0];
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//call 5 day weather
+async function getForecast(forecastURL) {
+  try {
+    const forecastResponse = await fetch(forecastURL);
+    const forecastData = await forecastResponse.json();
+    console.log("forecast", forecastData);
+
+    const icon = forecastData.list[0].icon;
+    const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    const t = forecastData.list;
+    // console.log(forecastData.list);
+    for (let i = 0; i < 5; i++) {
+      forecastStored.push(t[i]);
+    }
+    console.log(forecastStored);
+    //     const w = forecastData.wind.speed;
+    //     const h = forecastData.main.humidity;
+    //     forecastStored.push({
+    //       img: iconURL,
+    //       t: t,
+    //       w: w,
+    //       h: h,
+    //     });
+
+    //     return forecastData[0];
+  } catch (error) {
+    console.error(error);
+  }
+}
